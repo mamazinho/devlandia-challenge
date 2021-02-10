1c959dd1d3d7b0a2b9785c6317969a54420146d2
matheus <matheus@tradenamao.com.br>
2021-02-09 22:15:11 -0300

    finished

--- main.rb
+++ main.rb
@@ -1,31 +1,44 @@
-def get_princess_position(size, grid)
-    princess_position = []
-    (0...size).each do |row|
-        (0...size).each do |column|
-            if grid[row][column] == 'p'
-                princess_position.push(row)
-                princess_position.push(column)
+def draw_initial_grid(princess_position, my_position, size)
+    grid = Array.new
+
+    (0...size[0]).each do |row|
+        grid[row] = Array.new
+
+        (0...size[1]).each do |column|
+            if row == princess_position[0] && column == princess_position[1]
+                grid[row][column] = "p"
+                next
             end
+            if row == my_position[0] && column == my_position[1]
+                grid[row][column] = "m"
+                next
+            end
+            grid[row][column] = "-"
         end
+
     end
-    princess_position
+
+    grid.each do |line|
+        puts line.join(", ").gsub(",", "")
+    end
+
 end
 
 def process_move(princess_position, my_position)
 
-    # my_position and princess: 0 is the line and 1 is the column
+    # Positions: 0 is the line and 1 is the column
     new_position = my_position
     if princess_position[0] == my_position[0] and princess_position[1] < my_position[1]
-        move = 'LEFT'
+        move = "LEFT"
         new_position[1] = my_position[1] - 1 
     elsif princess_position[0] == my_position[0] and princess_position[1] > my_position[1]
-        move = 'RIGHT'
+        move = "RIGHT"
         new_position[1] = my_position[1] + 1 
     elsif princess_position[0] < my_position[0]
-        move = 'UP'
+        move = "UP"
         new_position[0] = my_position[0] - 1 
     else
-        move = 'DOWN'
+        move = "DOWN"
         new_position[0] = my_position[0] + 1 
     end
     
@@ -44,29 +57,39 @@ def nextMove(princess_position, my_position, moves=[])
     return moves
 end
 
-# NxN, the size is the N (always is a square)
-puts "Qual o numero de colunas e linhas? \n"
-size = gets.chomp.to_i
-puts "\nEm qual linha você está?\n"
-my_line = gets.chomp.to_i
-puts "\nEm qual coluna você está?\n"
-my_column = gets.chomp.to_i
-my_position = [my_line, my_column]
+if __FILE__ == $0
+    size = Array.new(2)
 
-grid = Array.new(size)
+    puts "Qual o numero de linhas da matriz? \n"
+    size[0] = gets.chomp.to_i
 
-(0...size).each do |i|
-    # This input shold be '-----' or '--m-p-'
-    grid[i] = gets
-end
+    puts "\nQual o numero de colunas da matriz? \n"
+    size[1] = gets.chomp.to_i
 
-princess_position = get_princess_position(size, grid)
-moves = nextMove(princess_position, my_position)
+    puts "\nEm qual linha você está? (OBS: 0 conta como a primeira linha)\n"
+    my_row = gets.chomp.to_i
 
-puts "All moves #{moves}"
+    puts "\nEm qual coluna você está? (OBS: 0 conta como a primeira coluna)\n"
+    my_column = gets.chomp.to_i
+    puts "\n"
 
-begin
-    puts "Last Move#{moves.last}"
-rescue
-    puts "Has no moves"
+    my_position = [my_row, my_column]
+
+    if my_position[0] >= size[0] || my_position[1] >= size[1]
+        puts "A posição que você se colocou está fora do tamanho máximo da matriz"
+        return
+    end
+
+    princess_position = [rand(0...size[0]), rand(0...size[1])]
+
+    draw_initial_grid(princess_position, my_position, size)
+    moves = nextMove(princess_position, my_position)
+
+    puts "\nAll moves #{moves}"
+
+    begin
+        puts "Last Move #{moves.last}"
+    rescue
+        puts "Has no moves"
+    end
 end
